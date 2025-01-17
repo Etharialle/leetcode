4e1e0f8804bec5d2114d63ae037c7aeb8d240d9d
etharialle <christopher.m.adams2@gmail.com>
2025-01-17 14:04:45 -0800

    need to do O(log(m+n))

---
+++ 4-median-of-two-sorted-arrays/4_log.py
@@ -0,0 +1,16 @@
+# inputs
+nums1 = [1,2,5,6]
+nums2 = [3,4,7,8]
+
+firstArrayLength = len(nums1)
+firstRight = nums1[(firstArrayLength // 2) -1]
+print(firstRight)
+
+
+
+secondArrayLength = len(nums2)
+secondLeft = nums2[(firstArrayLength // 2)- 1]
+print(secondLeft)
+
+firstLeft = nums1[(firstArrayLength // 2)]
+print(firstLeft)
