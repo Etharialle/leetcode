bc4a3403b8e06b8a31a5b24a8f2e16476d9d521d
etharialle <christopher.m.adams2@gmail.com>
2025-01-17 12:38:42 -0800

    brute force

---
+++ 3-longest-substring-without-repeating-characters/3.py
@@ -0,0 +1,34 @@
+# inputs
+s = " "
+
+# brute force
+# 1) initialize empty list
+# 2) for loop to check if character is in list already if not increment length
+# 3) remove first character from string, repeat 1 and 2, then remove second character until lenth of string either 0 or less than max length found
+"""
+maxLength = 0
+stringLength = len(s)
+while stringLength > 0:
+    lookup = []
+    length = 0
+    for char in range(len(s)):
+        if s[char] not in lookup:
+            length += 1
+            lookup.append(s[char])
+        else:
+            break
+    if length > maxLength:
+        maxLength = length
+    s = s.replace(s[0], "", 1)
+    stringLength = len(s)
+    if stringLength <= maxLength:
+        break
+
+print(maxLength)
+# return maxLength
+"""
+
+# optimized using the same idea as add two numbers
+lookup = {}
+for index, char in enumerate(s):
+
