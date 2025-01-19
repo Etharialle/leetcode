fd901f60398f9699dadf96cbe40443176038b2de
etharialle <christopher.m.adams2@gmail.com>
2025-01-19 10:20:09 -0800

    LC 49

--- 49-group-anagrams/49.py
+++ 49-group-anagrams/49.py
@@ -1,24 +1,29 @@
 # inputs
 strs = ["eat","tea","tan","ate","nat","bat"]
+strs = [""]
+strs = ["a"]
 
 # Problem Statement: Given an array of strings strs, group the 
 # anagrams together. You can return the answer in any order.
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    # create a hashmap for each string
-    result = {}
+    result = []
 
-    for i in range(len(strs)):
-        lookup = {}
-        word = strs[i]
-        print(word)
-        for j in range(len(word)):
-            if word[j] not in lookup.keys():
-                lookup[word[j]] = 1
-            else:
-                lookup[word[j]] += 1
-        print(lookup)
-        if lookup not in result.keys():
-            result[lookup] = 0
+    lookup = {}
+    for x in strs:
+        x_list = list(x)
+        x_list.sort()
+        x_tuple = tuple(x_list)
+        if x_tuple not in lookup:
+            lookup[x_tuple] = [x]
+        else:
+            current_list = []
+            current_list = lookup[x_tuple]
+            current_list.append(x)
+            lookup[x_tuple] = current_list
+    for x in lookup.values():
+        result.append(x)
+    return result
 
-groupAnagrams(strs)
+
+print(groupAnagrams(strs))
 
